943762861d372a79f744d633adc9d95f77c4506d
p0nyqz <93746632+p0nyqz@users.noreply.github.com>
2024-01-21 02:03:54 +0100

    component: Banner added

---
+++ src/app/components/banner.js
@@ -0,0 +1,12 @@
+const Banner = () => {
+  return (
+    <header>
+      <div>
+        <img src="./GloboLogo.png" alt="logo"></img>
+      </div>
+      <div>Providing houses all over the world</div>
+    </header>
+  )
+}
+
+export default Banner
