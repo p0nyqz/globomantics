b386f4ca80116a4f32a14f602a8faa6771581264
p0nyqz <93746632+p0nyqz@users.noreply.github.com>
2024-01-21 09:17:37 +0100

    feat: bootstrap and styles to banner added

--- src/app/components/banner.js
+++ src/app/components/banner.js
@@ -1,10 +1,20 @@
+import { logo } from './banner.module.css'
+
+const subtitleStyle = {
+  fontStyle: 'Italic',
+  fontSize: 'x-large',
+  color: 'coral',
+}
+
 const Banner = () => {
   return (
-    <header>
-      <div>
-        <img src="./GloboLogo.png" alt="logo"></img>
+    <header className="row mb-4">
+      <div className="col-5">
+        <img src="./globo-logo.png" alt="logo" className={logo}></img>
       </div>
-      <div>Providing houses all over the world</div>
+      <div className="col-7 mt-7" style={subtitleStyle}>
+        Providing houses all over the world
+      </div>
     </header>
   )
 }
