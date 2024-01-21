8816635158d8d9390a0978be6c74d354f7d13dbe
p0nyqz <93746632+p0nyqz@users.noreply.github.com>
2024-01-21 10:23:53 +0100

    component: houseList

---
+++ src/app/components/houseList.js
@@ -0,0 +1,46 @@
+import HouseRow from './houseRow'
+
+const houses = [
+  {
+    id: 1,
+    address: '12 Valey of Kings, Geneva',
+    country: 'Switzerland',
+    price: 900000,
+  },
+  {
+    id: 2,
+    address: '89 Road of Forks, Bern',
+    country: 'Switzerland',
+    price: 500000,
+  },
+]
+
+const HouseList = () => {
+  return (
+    <>
+      <div>
+        <div className="row mb-2">
+          <h5 className="themeFontColor text-center">
+            Houses currently on the market
+          </h5>
+        </div>
+        <table className="table table-hover">
+          <thead>
+            <tr>
+              <th>Address</th>
+              <th>Country</th>
+              <th>Asking Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            {houses.map((h) => (
+              <HouseRow key={h.id} house={h} />
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  )
+}
+
+export default HouseList
