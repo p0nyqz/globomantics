97502aeee4a7d0d43435a4da83c42179a693ab28
p0nyqz <93746632+p0nyqz@users.noreply.github.com>
2024-02-09 03:46:07 +0100

    feat: conditional rendering

--- src/app/components/houseRow.js
+++ src/app/components/houseRow.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import currencyFormatter from '../helpers/currencyFormatter'
 
 const HouseRow = ({ house }) => {
@@ -5,9 +6,15 @@ const HouseRow = ({ house }) => {
     <tr>
       <td>{house.address}</td>
       <td>{house.country}</td>
-      <td>{currencyFormatter.format(house.price)}</td>
+      {house.price && (
+        <td className={`${house.price >= 500000 ? 'text-primary' : ''}`}>
+          {currencyFormatter.format(house.price)}
+        </td>
+      )}
     </tr>
   )
 }
+
+const HouseRowMem = React.memo(HouseRow)
 
 export default HouseRow
