aa76a450441510b39a6b798bf8ba8765cbe056b8
p0nyqz <93746632+p0nyqz@users.noreply.github.com>
2024-02-09 04:04:38 +0100

    feat: App selectedHouse

--- src/app/components/app.js
+++ src/app/components/app.js
@@ -1,11 +1,16 @@
+'use client'
+
+import { useState } from 'react'
 import Banner from './banner'
 import HouseList from './houseList'
+import House from './house'
 
 const App = () => {
+  const [selectedHouse, setSelectedHouse] = useState()
   return (
     <>
       <Banner headerText="Providing houses all over the world" />
-      <HouseList />
+      {selectedHouse ? <House house={selectedHouse} /> : <HouseList />}
     </>
   )
 }
